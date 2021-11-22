02f535f7e1f3f50a573a3f0cabbc817d0dd3d68d
chulocr8v <nkematu5@gmail.com>
2021-11-22 20:01:30 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menu = document.querySelector(".menu");
+const header = document.querySelector("header");
 
+
+/* display mobile menu */
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("rotate");
   menu.classList.toggle("show-menu");
 });
 
-//x = 400;
-//alert(x / 16);
+
+/* add shadow to header on scroll */
+const headerShadow = () => {
+  let prevPos = 100;
+  let currentPos = scrollY;
+
+  if (currentPos > prevPos) {
+    header.classList.add('header-shadow')
+  } else {
+    header.classList.remove('header-shadow')
+  }
+}
+
+window.addEventListener('scroll', headerShadow)
